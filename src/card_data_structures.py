ff9a613e592109c6a7df92cb41fb65052d5592d5
Kevin Xia <kevinmxia@gmail.com>
2021-03-01 20:04:31 -0800

    Add card, card collection, and deck data structures.

---
+++ src/card_data_structures.py
@@ -0,0 +1,87 @@
+import random
+
+
+allowed_ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+allowed_suits = ['C', 'D', 'H', 'S']
+
+
+class Card:
+    def __init__(self, rank, suit):
+        suit = suit.upper()
+
+        assert rank in allowed_ranks
+        assert suit in allowed_suits
+
+        self.rank = rank
+        self.suit = suit
+
+    def parse_card(self, mode="default"):
+        if mode == "default":
+            r = self.rank
+            if r == 1:
+                r = 'A'
+            elif r == 10:
+                r = 'T'
+            elif r == 11:
+                r = 'J'
+            elif r == 12:
+                r = 'Q'
+            elif r == 13:
+                r = 'K'
+            return "{}{}".format(r, self.suit)
+        else:
+            raise Exception("Mode \"{}\" for parsing cards does not exist.".format(mode))
+
+    def __str__(self):
+        return self.parse_card(mode="default")
+
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            if self.rank == other.rank and self.suit == other.suit:
+                return True
+        return False
+
+    def __hash__(self):
+        return hash(str(self))
+
+
+class CardCollection:
+    def __init__(self, cards=None):
+        if cards is None:
+            self.cards = []
+        else:
+            self.cards = cards
+        self.card_set = set(self.cards)
+
+    def __str__(self):
+        return str([str(c) for c in self.cards])
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def contains(self, card):
+        return card in self.card_set
+
+    def add_top(self, card):
+        self.card_set.add(card)
+        self.cards.insert(0, card)
+
+    def add_bottom(self, card):
+        self.card_set.add(card)
+        self.cards.append(card)
+
+    def remove(self, card):
+        if card in self.card_set:
+            self.card_set.remove(card)
+            self.cards.remove(card)
+            return True
+        return False
+
+
+class Deck(CardCollection):
+    def __init__(self):
+        deck = []
+        for suit in allowed_suits:
+            for rank in allowed_ranks:
+                deck.append(Card(rank, suit))
+        super().__init__(deck)
